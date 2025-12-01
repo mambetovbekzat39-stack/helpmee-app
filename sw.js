6c90b43d55248b56528554c2438300e27ededcb6
mambetovbekzat39 <mambetovbekzat39@gmail.com>
2025-12-02 00:16:05 +0600

    изменил опять код

--- sw.js
+++ sw.js
@@ -2,13 +2,13 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('allah99-cache').then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/icon-192.png',
-        '/icon-512.png',
-        '/splash.png'
+        '.',
+        'index.html',
+        'style.css',
+        'script.js',
+        'icon-192.png',
+        'icon-512.png',
+        'splash.png'
       ]);
     })
   );
