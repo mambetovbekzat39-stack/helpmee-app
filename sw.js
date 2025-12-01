5d2bb908c2e644c1c9223f87b78aab70ccc8fc77
mambetovbekzat39 <mambetovbekzat39@gmail.com>
2025-12-02 00:18:51 +0600

    опять опять изменил  код

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('allah99-cache').then(cache => {
-      return cache.addAll([
-        '.',
-        'index.html',
-        'style.css',
-        'script.js',
-        'icon-192.png',
-        'icon-512.png',
-        'splash.png'
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open('allah99-cache').then(cache => {
+            return cache.addAll([
+                '.',
+                'index.html',
+                'style.css',
+                'script.js',
+                'icon-192.png',
+                'icon-512.png',
+                'splash.png'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
 });
